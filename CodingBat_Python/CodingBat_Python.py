d6563358d1ccf9c031cae0f8277369a912c6e041
quickm.career <quickm.career@gmail.com>
2021-02-21 14:30:37 -0800

    Add algorithm script and test function for code challenge sum_double.
    Added empty py file for other code challenges in Warmup 1 category

--- CodingBat_Python/CodingBat_Python.py
+++ CodingBat_Python/CodingBat_Python.py
@@ -2,9 +2,11 @@
 
 from test_challenge import test_sleep_in
 from test_challenge import test_monkey_trouble
+from test_challenge import test_sum_double
 
 print("Welcome to CoderBat - Python code challeges")
 print()
 
 test_sleep_in()
 test_monkey_trouble()
+test_sum_double()
--- CodingBat_Python/test_challenge.py
+++ CodingBat_Python/test_challenge.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 from  Warmup_1.sleep_in       import sleep_in
 from  Warmup_1.monkey_trouble import monkey_trouble
+from  Warmup_1.sum_double     import sum_double
 
 def test_sleep_in():
   challenge = "Sleep In"
@@ -47,3 +48,38 @@ def test_monkey_trouble():
   print("Input : Weekend: %r   Vacation: %r" %(a_smile, b_smile))
   print("Output: %r" %(monkey_trouble(a_smile, b_smile)))
   print()
+
+def test_sum_double():
+  challenge = "Sleep In"
+
+  print("Code Challenge: %s: " %(challenge))
+
+# Examples:
+# sum_double(1, 2) → 3
+# sum_double(3, 2) → 5
+# sum_double(2, 2) → 8
+
+
+  a = 1
+  b = 2
+
+  print("Input : First: %d   Second: %d" %(a, b))
+  print("Output: %d" %(sum_double(a, b)))
+  print()
+
+  a = 3
+  b = 2
+
+  print("Input : First: %d   Second: %d" %(a, b))
+  print("Output: %d" %(sum_double(a, b)))
+  print()
+
+  a = 2
+  b = 2
+
+  print("Input : First: %d   Second: %d" %(a, b))
+  print("Output: %d" %(sum_double(a, b)))
+  print()
+
+
+
