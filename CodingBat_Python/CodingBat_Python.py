71d59874045f10f855c93e17f4d9a75d376d5f69
quickm.career <quickm.career@gmail.com>
2021-02-21 16:18:18 -0800

    Added algorithm and test function for code challenge Parrot Trouble

--- CodingBat_Python/CodingBat_Python.py
+++ CodingBat_Python/CodingBat_Python.py
@@ -3,6 +3,9 @@
 from test_challenge import test_sleep_in
 from test_challenge import test_monkey_trouble
 from test_challenge import test_sum_double
+from test_challenge import test_diff21
+from test_challenge import test_parrot_trouble
+
 
 print("Welcome to CoderBat - Python code challeges")
 print()
@@ -10,3 +13,5 @@ print()
 test_sleep_in()
 test_monkey_trouble()
 test_sum_double()
+test_diff21()
+test_parrot_trouble()
--- CodingBat_Python/test_challenge.py
+++ CodingBat_Python/test_challenge.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python
-from  Warmup_1.sleep_in       import sleep_in
-from  Warmup_1.monkey_trouble import monkey_trouble
-from  Warmup_1.sum_double     import sum_double
+from Warmup_1.sleep_in       import sleep_in
+from Warmup_1.monkey_trouble import monkey_trouble
+from Warmup_1.sum_double     import sum_double
+from Warmup_1.diff21         import diff21
+from Warmup_1.parrot_trouble import parrot_trouble
+
+
+
+def print_challenge_name(challenge_name):
+    challenge = challenge_name
+    print("Code Challenge: %s: " %(challenge))
 
 def test_sleep_in():
-  challenge = "Sleep In"
-
-  print("Code Challenge: %s: " %(challenge))
+  print_challenge_name("Sleep In")
 
   weekday   = False
   vacation  = False
@@ -50,36 +56,63 @@ def test_monkey_trouble():
   print()
 
 def test_sum_double():
-  challenge = "Sleep In"
-
-  print("Code Challenge: %s: " %(challenge))
-
-# Examples:
-# sum_double(1, 2) → 3
-# sum_double(3, 2) → 5
-# sum_double(2, 2) → 8
-
+  print_challenge_name("Sum Double")
 
   a = 1
   b = 2
-
   print("Input : First: %d   Second: %d" %(a, b))
   print("Output: %d" %(sum_double(a, b)))
   print()
 
   a = 3
   b = 2
-
   print("Input : First: %d   Second: %d" %(a, b))
   print("Output: %d" %(sum_double(a, b)))
   print()
 
   a = 2
   b = 2
-
   print("Input : First: %d   Second: %d" %(a, b))
   print("Output: %d" %(sum_double(a, b)))
   print()
 
+def test_diff21():
+  print_challenge_name("Difference 21")
+
+  num = 19
+  print("Input : %d" %(num))
+  print("Output: %d" %(diff21(num)))
+  print()
+
+  num = 10
+  print("Input : %d" %(num))
+  print("Output: %d" %(diff21(num)))
+  print()
+
+  num = 21
+  print("Input : %d" %(num))
+  print("Output: %d" %(diff21(num)))
+  print()
+
+def test_parrot_trouble():
+  print_challenge_name("Parrot Trouble")
+
+  talking = True
+  hour    = 6
+  print("Input : Talking: %r   Hour: %d" %(talking, hour))
+  print("Output: %r" %(parrot_trouble(talking, hour)))
+  print()
+
+  talking = True
+  hour    = 7
+  print("Input : Talking: %r   Hour: %d" %(talking, hour))
+  print("Output: %r" %(parrot_trouble(talking, hour)))
+  print()
+
+  talking = False
+  hour    = 6
+  print("Input : Talking: %r   Hour: %d" %(talking, hour))
+  print("Output: %r" %(parrot_trouble(talking, hour)))
+  print()
 
 
